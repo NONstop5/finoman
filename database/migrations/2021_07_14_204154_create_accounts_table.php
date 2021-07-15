255c88293bf14d0f482e258bedc378df8ca7b939
scarymovie <81684836+scarymovie@users.noreply.github.com>
2021-07-15 13:32:55 +0300

    add migrations

---
+++ database/migrations/2021_07_14_204154_create_accounts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')
+                ->unsigned()
+                ->index();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+            $table->bigInteger('types_id')
+                ->unsigned()
+                ->index();
+            $table->foreign('types_id')
+                ->references('id')
+                ->on('types');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
