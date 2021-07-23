01f9a1d765cab9e98e70ee0ceaf6ddf7ca358efa
NONstop <non_2002@mail.ru>
2021-07-23 16:07:54 +0300

    #19 fix api.php to remove login route

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,10 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', [LoginController::class, 'authenticate']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-
+Route::middleware('auth:sanctum')->get(
+    '/user',
+    function (Request $request) {
+        return $request->user();
+    }
+);
