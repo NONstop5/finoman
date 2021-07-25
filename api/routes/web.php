484f11afeeacefef440734f7e384c0a3b4ce03ef
NONstop <non_2002@mail.ru>
2021-07-26 02:52:14 +0300

    #19 refactor web.php

--- api/routes/web.php
+++ api/routes/web.php
@@ -13,7 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('{all}', 'app')
-    ->where(['all' => '^(?!api).*$']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
-//Route::view('/{any}', 'app')->where('any','.*');
+// Route::view('{all}', 'app')
+//     ->where(['all' => '^(?!api).*$']);
+//
+// //Route::view('/{any}', 'app')->where('any','.*');
+//
+// Route::get('{any}', function () {
+//     return view('app');
+// })->where('any','.*');
