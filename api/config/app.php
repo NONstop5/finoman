016ca8bbd36d6ccce32330597e03b3181c62884b
NONstop <non_2002@mail.ru>
2021-07-30 14:12:49 +0300

    fix api.php to exclude fortify

--- api/config/app.php
+++ api/config/app.php
@@ -165,7 +165,6 @@ return [
         /*
          * Package Service Providers...
          */
-        App\Providers\FortifyServiceProvider::class,
 
         /*
          * Application Service Providers...
