84b17601452e33745ac68c8a502bbaeb72fbb60a
NONstop <non_2002@mail.ru>
2021-07-30 23:59:18 +0300

    reformat mutations.js

--- quasarapp/src/store/user/mutations.js
+++ quasarapp/src/store/user/mutations.js
@@ -21,17 +21,21 @@ function setToken(state, value) {
 function ADD_WALLET(state, wallet) {
   state.wallets.push(wallet);
 }
+
 function GET_WALLETS(state, wallets) {
   state.wallets = wallets;
 }
+
 function UPDATE_WALLET(state, wallet) {
-  const index = state.wallets.findIndex(h => h.id === wallet.id);
+  const index = state.wallets.findIndex((h) => h.id === wallet.id);
   state.wallets.splice(index, 1, wallet);
   state.wallets = [...state.wallets];
 }
+
 function DELETE_WALLET(state, wallet) {
-  state.wallets = [...state.wallets.filter(p => p.id !== wallet.id)];
+  state.wallets = [...state.wallets.filter((p) => p.id !== wallet.id)];
 }
+
 // export default {
 //   SET_WALLET: (state, payload) => {
 //     state.wallets = payload;
@@ -40,6 +44,7 @@ function DELETE_WALLET(state, wallet) {
 //     state.wallets.push(payload);
 //   }
 // }
+
 export {
   setLoggedIn,
   setDetails,
