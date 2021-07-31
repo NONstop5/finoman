f89094b2ee80b219edbab5244ace12c529837b3e
NONstop <non_2002@mail.ru>
2021-07-31 23:40:58 +0300

    add CategoryController.php, WalletController.php

---
+++ api/app/Http/Controllers/WalletController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Wallet;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class WalletController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Wallet $wallet
+     *
+     * @return Response
+     */
+    public function show(Wallet $wallet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request  $request
+     * @param Wallet $wallet
+     *
+     * @return Response
+     */
+    public function update(Request $request, Wallet $wallet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Wallet  $wallet
+     *
+     * @return Response
+     */
+    public function destroy(Wallet $wallet)
+    {
+        //
+    }
+}
