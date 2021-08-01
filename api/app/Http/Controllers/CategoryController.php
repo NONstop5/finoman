306a60e17b1ad5191cfdb69913c9c47f7eb76c78
NONstop <non_2002@mail.ru>
2021-08-01 05:30:11 +0300

    fix CategoryController.php to get categories list

--- api/app/Http/Controllers/CategoryController.php
+++ api/app/Http/Controllers/CategoryController.php
@@ -3,19 +3,39 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return Response
+     * @param Request $request
+     *
+     * @return JsonResponse
+     * @throws ValidationException
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $validated = $this->validate(
+            $request,
+            [
+                'category_type_id' => 'nullable|integer|exists:category_types,id',
+            ]
+        );
+
+        $qb = Category::query();
+
+        if (isset($validated['category_type_id'])) {
+            $qb->where('category_type_id', $validated['category_type_id']);
+        }
+
+        $categoriesList = $qb->get();
+
+        return response()->json($categoriesList);
     }
 
     /**
@@ -45,7 +65,7 @@ class CategoryController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param Request  $request
+     * @param Request $request
      * @param Category $category
      *
      * @return Response
@@ -58,7 +78,7 @@ class CategoryController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param Category  $category
+     * @param Category $category
      *
      * @return Response
      */
