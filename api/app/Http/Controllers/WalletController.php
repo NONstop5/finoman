9874e9e529248bd894673b67aea21c3b190e6bcb
NONstop <non_2002@mail.ru>
2021-08-01 05:30:36 +0300

    fix WalletController.php to get wallets list

--- api/app/Http/Controllers/WalletController.php
+++ api/app/Http/Controllers/WalletController.php
@@ -2,20 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Wallet;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 
 class WalletController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return Response
+     * @param Request $request
+     *
+     * @return JsonResponse
+     * @throws ValidationException
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $validated = $this->validate(
+            $request,
+            [
+                'wallet_type_id' => 'nullable|integer|exists:wallet_types,id',
+            ]
+        );
+
+        $qb = Category::query();
+
+        if (isset($validated['wallet_type_id'])) {
+            $qb->where('wallet_type_id', $validated['wallet_type_id']);
+        }
+
+        $walletsList = $qb->get();
+
+        return response()->json($walletsList);
     }
 
     /**
@@ -45,7 +66,7 @@ class WalletController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param Request  $request
+     * @param Request $request
      * @param Wallet $wallet
      *
      * @return Response
@@ -58,7 +79,7 @@ class WalletController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param Wallet  $wallet
+     * @param Wallet $wallet
      *
      * @return Response
      */
