89c93aa54c236cb72d634fcd78b121f4dcc315e9
NONstop <non_2002@mail.ru>
2021-08-01 03:04:55 +0300

    add UserSeeder.php

---
+++ api/database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert(
+            [
+                'name' => 'John Doe',
+                'email' => 'john_doe@finoman.ru.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'), //'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+                'remember_token' => Str::random(10),
+            ]
+        );
+    }
+}
