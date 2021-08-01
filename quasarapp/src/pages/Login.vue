9d21115841fe2f937b473c58e65942e60b51c344
NONstop <non_2002@mail.ru>
2021-08-01 03:04:22 +0300

    fix Login.vue to change default login value

--- quasarapp/src/pages/Login.vue
+++ quasarapp/src/pages/Login.vue
@@ -57,7 +57,7 @@ export default {
   data() {
     return {
       formData: {
-        email: 'dylan82@example.org',
+        email: 'john_doe@finoman.ru.com',
         password: 'password',
       },
     };
