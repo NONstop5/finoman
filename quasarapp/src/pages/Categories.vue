d0d1ed0d7bae4b38b5311f53996fb4b3f07b1878
DiskoCisko <53272759+DiskoCisko@users.noreply.github.com>
2021-08-02 23:50:18 +0300

    add Categories

--- quasarapp/src/pages/Categories.vue
+++ quasarapp/src/pages/Categories.vue
@@ -7,14 +7,61 @@
       <q-separator
         class="q-mb-lg"
       />
+      <div>
+        <q-card
+          v-for="category in categories"
+          :key="category.id"
+          flat
+          class="flex justify-start items-center q-mb-sm"
+        >
+          <q-icon
+            :name="category.icon"
+            :size="'1.5em'"
+            @click="click"
+          />
+          <div class="q-ml-lg">
+            <div class="text-subtitle2">
+              {{ category.name }}
+            </div>
+            <div class="text-subtitle2">
+              {{ showDate(category.created_at) }}
+            </div>
+          </div>
+        </q-card>
+        <q-space />
+      </div>
     </div>
   </q-page>
 </template>
 
 <script>
+import {
+  mapActions,
+  mapState,
+} from 'vuex';
+import { date } from 'quasar';
+
 export default {
   name: 'Categories',
   data: () => ({}),
+  computed: {
+    ...mapState('user', ['wallets', 'categories', 'transactions']),
+  },
+  async created() {
+    await this.loadInfo();
+  },
+  methods: {
+    ...mapActions('user', ['getWalletsAction', 'getCategoriesAction', 'getTransactionsAction']),
+    async loadInfo() {
+      await this.getWalletsAction();
+      await this.getCategoriesAction();
+      await this.getTransactionsAction();
+    },
+    showDate(dt) {
+      const timeStamp = new Date(dt);
+      return date.formatDate(timeStamp, 'YYYY-MM-DD HH:mm');
+    },
+  },
 };
 </script>
 
