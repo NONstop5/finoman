78eada6c81a63e28bd36bcfa4dd2b57b15ae4b13
NONstop <non_2002@mail.ru>
2021-08-02 05:26:29 +0300

    refactor Login.vue

--- quasarapp/src/pages/Login.vue
+++ quasarapp/src/pages/Login.vue
@@ -1,38 +1,36 @@
 <template>
   <q-page class="row justify-center items-center content-center">
-    <div class="col-xs-12 col-sm-6 col-md-3 text-center">
-      <div class="q-gutter-lg">
-        <q-input
-          v-model="formData.email"
-          outlined
-          type="email"
-          placeholder="Email"
-          name="email"
-          :model-value="formData.email"
-        />
-        <q-input
-          v-model="formData.password"
-          outlined
-          type="password"
-          placeholder="Password"
-          name="password"
-          :model-value="formData.password"
-        />
-        <q-btn
-          size="lg"
-          color="secondary"
-          @click="onSubmit"
-        >
-          Login
-        </q-btn>
-        <q-btn
-          color="primary"
-          size="lg"
-          to="/registration"
-        >
-          Sign Up
-        </q-btn>
-      </div>
+    <div class="col-xs-12 col-sm-6 q-gutter-lg col-md-3 text-center">
+      <q-input
+        v-model="formData.email"
+        outlined
+        type="email"
+        placeholder="Email"
+        name="email"
+        :model-value="formData.email"
+      />
+      <q-input
+        v-model="formData.password"
+        outlined
+        type="password"
+        placeholder="Password"
+        name="password"
+        :model-value="formData.password"
+      />
+      <q-btn
+        size="lg"
+        color="secondary"
+        @click="onSubmit"
+      >
+        Login
+      </q-btn>
+      <q-btn
+        color="primary"
+        size="lg"
+        to="/register"
+      >
+        Sign Up
+      </q-btn>
     </div>
   </q-page>
 </template>
