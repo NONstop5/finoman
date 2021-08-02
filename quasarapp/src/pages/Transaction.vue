fe42627d63e3ddac8e4448d0c4e749e3a45ac827
DiskoCisko <53272759+DiskoCisko@users.noreply.github.com>
2021-08-02 22:44:54 +0300

    fix eslint warring

--- quasarapp/src/pages/Transaction.vue
+++ quasarapp/src/pages/Transaction.vue
@@ -1,70 +1,87 @@
 <template>
   <q-page class="container main">
-    <h4 class="text-secondary">
+    <h3 class="text-secondary">
       Transaction
-    </h4>
+    </h3>
     <div class="wallets-container">
-    <div class="q-pa-md">
-      <q-form
-        @submit="onSubmit"
-      >
-      <q-btn-toggle
-        color="secondary"
-        v-model="transaction_types_id"
-        toggle-color="info"
-        :options="[
-          {label: 'Income', value: 1},
-          {label: 'Transfer', value: 3},
-          {label: 'Outcome', value: 2}
-        ]"
-      />
-      <div>
-        <q-select
-          class="text-secondary q-my-lg"
-          outlined
-          v-model="model"
-          :options="options"
-          label="Select category"
-          :rules="[ val => val && val.length > 0 || 'Please type something']"
-        />
-      </div>
-      <div>
-        <q-input
-          :rules="[ val => val && val.length > 0 || 'Please type something']"
-          class="text-secondary q-my-lg"
-          outlined
-          v-model="text"
-          label="Value"
-        />
-      </div>
-      <div>
-        <q-input
-          class="text-secondary q-my-lg"
-          outlined
-          v-model="date"
-          mask="date"
-          :rules="['date']"
+      <div class="q-pa-md">
+        <q-form
+          @submit="onSubmit"
         >
-          <q-icon name="event" size="md" class="cursor-pointer q-pt-md">
-            <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
-              <q-date
-                color="secondary"
-                class="text-secondary"
-                v-model="date"
+          <q-btn-toggle
+            v-model="transaction_types_id"
+            color="secondary"
+            toggle-color="info"
+            :options="[
+              {label: 'Income', value: 1},
+              {label: 'Transfer', value: 3},
+              {label: 'Outcome', value: 2}
+            ]"
+          />
+          <div>
+            <q-select
+              v-model="model"
+              class="text-secondary q-my-lg"
+              outlined
+              :options="options"
+              label="Select category"
+              :rules="[ val => val && val.length > 0 || 'Please type something']"
+            />
+          </div>
+          <div>
+            <q-input
+              v-model="text"
+              :rules="[ val => val && val.length > 0 || 'Please type something']"
+              class="text-secondary q-my-lg"
+              outlined
+              label="Value"
+            />
+          </div>
+          <div>
+            <q-input
+              v-model="date"
+              class="text-secondary q-my-lg"
+              outlined
+              mask="date"
+              :rules="['date']"
+            >
+              <q-icon
+                name="event"
+                size="md"
+                class="cursor-pointer q-pt-md"
               >
-                <div class="row items-center justify-end">
-                  <q-btn v-close-popup label="Close" color="secondary" flat />
-                </div>
-              </q-date>
-            </q-popup-proxy>
-          </q-icon>
-      </q-input>
+                <q-popup-proxy
+                  ref="qDateProxy"
+                  transition-show="scale"
+                  transition-hide="scale"
+                >
+                  <q-date
+                    v-model="date"
+                    color="secondary"
+                    class="text-secondary"
+                  >
+                    <div class="row items-center justify-end">
+                      <q-btn
+                        v-close-popup
+                        label="Close"
+                        color="secondary"
+                        flat
+                      />
+                    </div>
+                  </q-date>
+                </q-popup-proxy>
+              </q-icon>
+            </q-input>
+          </div>
+          <div>
+            <q-btn
+              label="Submit"
+              type="submit"
+              color="secondary"
+            />
+          </div>
+        </q-form>
       </div>
-       <div>
-        <q-btn label="Submit" type="submit" color="secondary"/>
-      </div>
-    </q-form>
-  </div>
     </div>
     <div />
   </q-page>
