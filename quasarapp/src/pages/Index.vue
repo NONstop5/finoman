00b4c83835b81b3a2f36d404cac4fad328161bd2
NONstop <non_2002@mail.ru>
2021-08-02 05:02:54 +0300

    fix Index.vue to change markup and refactor

--- quasarapp/src/pages/Index.vue
+++ quasarapp/src/pages/Index.vue
@@ -1,87 +1,83 @@
 <template>
-  <q-page class="fit row wrap justify-center items-center content-start">
-    <div class="col-10">
-      <h4 class="text-primary">
-        Wallets
+  <q-page class="row justify-center items-center content-start">
+    <div class="col">
+      <h4 class="text-primary q-my-sm">
+        Summary
       </h4>
-      <div class="flex items-center">
+      <q-separator
+        class="q-mb-lg"
+      />
+      <div class="flex items-center q-gutter-sm q-mb-lg">
         <q-card
           v-for="wallet in wallets"
-          :key="wallet.userid"
-          class="text-white q-mr-md"
-          style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%);
-            max-width: 222px"
-        >
-          <q-card-section>
-            <div class="text-h6">
-              {{ wallet.type_id }}
-            </div>
-            <div class="text-subtitle2">
-              {{ wallet.userid }}
-            </div>
-          </q-card-section>
-        </q-card>
-        <q-btn
-          round
-          color="secondary"
+          :key="wallet.id"
+          class="text-center bg-primary glossy text-white q-pa-xs"
         >
           <q-icon
-            name="fas fa-plus"
+            :name="wallet.icon"
             :size="'1.5em'"
           />
-        </q-btn>
-      </div>
-      <div class="flex items-center q-mt-xl">
-        <div
-          v-for="category in categories"
-          :key="category.name"
-          class="text-center q-mr-lg q-mb-md"
-        >
-          <q-icon
-            :name="category.name"
-            :size="'1.5em'"
-          />
-          <div class="text-h6">
-            {{ category.title }}
+          <div class="text-subtitle2">
+            {{ wallet.name }}
           </div>
-        </div>
+        </q-card>
+        <q-space />
         <q-btn
           round
-          outline
           color="secondary"
+          icon="fas fa-plus"
+        />
+      </div>
+      <q-separator
+        class="q-mb-lg"
+      />
+      <div class="flex items-center q-gutter-sm q-mb-lg">
+        <q-card
+          v-for="category in categories"
+          :key="category.id"
+          class="text-center q-pa-xs"
         >
           <q-icon
-            name="fas fa-plus"
+            :name="category.icon"
             :size="'1.5em'"
           />
-        </q-btn>
+          <div class="text-subtitle2">
+            {{ category.name }}
+          </div>
+        </q-card>
+        <q-space />
+        <q-btn
+          round
+          color="secondary"
+          icon="fas fa-plus"
+        />
       </div>
-      <div class="flex justify-between items-center">
-        <h4 class="text-primary">
-          Last transaction
-        </h4>
-        <div
-          class="text-secondary cursor-pointer"
-        >
-          All >
-        </div>
-      </div>
+      <q-separator
+        class="q-mb-lg"
+      />
+      <h5 class="text-primary q-mb-sm q-mt-none">
+        Last transaction
+      </h5>
       <q-card
         v-for="transaction in transactions"
-        :key="transaction.name"
-        class="bg-dark flex justify-between items-center q-mb-lg"
+        :key="transaction.id"
+        flat
+        bordered
+        class="flex justify-between items-center q-mb-sm"
       >
         <q-item>
           <q-item-section avatar>
             <q-avatar>
               <q-icon
                 v-if="transaction.spending"
-                name="fas fa-chevron-down"
+                class="text-negative"
+                name="fas fa-chevron-up"
                 :size="'1.5em'"
               />
               <q-icon
                 v-else
-                name="fas fa-chevron-up"
+                class="text-positive"
+                name="fas fa-chevron-down"
                 :size="'1.5em'"
               />
             </q-avatar>
@@ -97,7 +93,7 @@
         <q-item>
           <q-item-label
             v-if="transaction.spending"
-            class="q-pt-sm"
+            class="q-pt-sm text-negative"
           >
             -{{ transaction.price }} ₽
           </q-item-label>
@@ -115,53 +111,91 @@
 
 <script>
 import { defineComponent } from 'vue';
-import { mapActions, mapState } from 'vuex';
+import {
+  mapActions,
+  mapState,
+} from 'vuex';
 
 export default defineComponent({
-  name: 'PageIndex',
-  data() {
-    return {
-      cards: [
-        {
-          name: 'card.name',
-          balanse: 'card.balanse',
-        },
-      ],
-      categories: [
-        {
-          title: 'Product',
-          name: 'fas fa-hamburger',
-        },
-        {
-          title: 'Product',
-          name: 'fas fa-hamburger',
-        },
-        {
-          title: 'Product',
-          name: 'fas fa-hamburger',
-        },
-      ],
-      transactions: [
-        {
-          name: 'Products',
-          date: '10 Jan, 10:55',
-          price: 1500,
-          spending: true,
-        },
-        {
-          name: 'Products',
-          date: '10 Jan, 10:55',
-          price: 1500,
-          spending: false,
-        },
-      ],
-    };
-  },
+  name: 'Index',
+  data: () => ({
+    wallets: [
+      {
+        id: 1,
+        name: 'Cash',
+        icon: 'fas fa-wallet',
+      },
+      {
+        id: 2,
+        name: 'Card VTB',
+        icon: 'fas fa-credit-card',
+      },
+      {
+        id: 3,
+        name: 'Card SBER',
+        icon: 'fas fa-credit-card',
+      },
+    ],
+    categories: [
+      {
+        id: 1,
+        name: 'Food',
+        icon: 'fas fa-shopping-cart',
+      },
+      {
+        id: 2,
+        name: 'Pets',
+        icon: 'fas fa-paw',
+      },
+      {
+        id: 3,
+        name: 'Transport',
+        icon: 'fas fa-car',
+      },
+    ],
+    transactions: [
+      {
+        id: 1,
+        name: 'Food',
+        date: '10 Jan, 10:55',
+        price: 1500,
+        spending: true,
+      },
+      {
+        id: 2,
+        name: 'Transport',
+        date: '10 Jan, 10:55',
+        price: 1500,
+        spending: false,
+      },
+      {
+        id: 3,
+        name: 'Pets',
+        date: '10 Jan, 10:55',
+        price: 1500,
+        spending: false,
+      },
+      {
+        id: 4,
+        name: 'Food',
+        date: '10 Jan, 10:55',
+        price: 1500,
+        spending: true,
+      },
+      {
+        id: 5,
+        name: 'Transport',
+        date: '10 Jan, 10:55',
+        price: 1500,
+        spending: false,
+      },
+    ],
+  }),
   computed: {
     ...mapState('user', ['transactions']),
   },
   async created() {
-    await this.loadWallets();
+    // await this.loadWallets();
   },
   methods: {
     ...mapActions('user', ['getTransactionsAction']),
