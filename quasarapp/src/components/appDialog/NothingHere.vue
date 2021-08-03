093ed2622c725cef211c6c3d5c883037690b5a5d
tornadosk <oleg.kulyba@gmail.com>
2021-08-03 19:57:55 -0400

    work on Add functionality, reusable dialog pages, do not merge.

---
+++ quasarapp/src/components/appDialog/NothingHere.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="nothing-here absolute-center text-center">
+        <q-icon
+            :name="icon"
+            size="100px"
+            color="light"
+        />
+        <div class="text-size-21 text-light">
+            {{ $t(label) }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['label', 'icon'],
+    setup(props) {
+        return {
+            label: props.label,
+            icon: props.icon,
+        }
+    }
+}
+</script>
---
+++ quasarapp/src/components/appDialog/wallets.vue
@@ -0,0 +1,4 @@
+<template>
+    <wallets-list v-if="store.user.state.wallets.length"/>
+    <nothing-here v-else icon="la la-wallet" label="no wallets yet"/>
+</template>
