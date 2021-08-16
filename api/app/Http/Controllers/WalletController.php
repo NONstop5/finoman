da667fdad2fbcf752c8b1b092fbf1be9f3e5174b
NONstop <non_2002@mail.ru>
2021-08-16 03:17:42 +0300

    fix WalletController.php to move code to request and service

--- api/app/Http/Controllers/WalletController.php
+++ api/app/Http/Controllers/WalletController.php
@@ -4,67 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\WalletRequest;
 use App\Models\Wallet;
+use App\Services\WalletService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\ValidationException;
 
 class WalletController extends Controller
 {
     /**
+     * @var WalletService
+     */
+    protected $walletService;
+
+    public function __construct(WalletService $walletService)
+    {
+        $this->walletService = $walletService;
+    }
+
+    /**
      * Display a listing of the resource.
      *
-     * @param Request $request
+     * @param WalletRequest $request
      *
      * @return JsonResponse
-     * @throws ValidationException
      */
-    public function index(Request $request)
+    public function index(WalletRequest $request)
     {
-        $validated = $this->validate(
-            $request,
-            [
-                'wallet_type_id' => 'nullable|integer|exists:wallet_types,id',
-            ]
-        );
-
-        $qb = Wallet::query()
-            ->where('user_id', Auth::id());
-
-        if (isset($validated['wallet_type_id'])) {
-            $qb->where('wallet_type_id', $validated['wallet_type_id']);
-        }
-
-        $walletsList = $qb->get();
-
-        return response()->json($walletsList);
+        return response()->json($this->walletService->getList($request->validated()));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param Request $request
+     * @param WalletRequest $request
      *
      * @return JsonResponse
      */
     public function store(WalletRequest $request): JsonResponse
     {
-        $validated = $request->validated();
-
-        return response()->json(
-            Wallet::query()
-                ->create([
-                    'user_id' => Auth::id(),
-                    'currency_id' => 1,
-                    'wallet_type_id' => $validated['wallet_type_id'],
-                    'name' => $validated['name'],
-                    'balance' => $validated['balance'],
-                    'balance_date' => $validated['balance_date'],
-                    'icon' => $validated['icon'],
-                ]),
-            Response::HTTP_CREATED
-        );
+        return response()->json($this->walletService->create($request->validated()), Response::HTTP_CREATED);
     }
 
     /**
@@ -82,22 +59,14 @@ class WalletController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param Request $request
+     * @param WalletRequest $request
      * @param Wallet $wallet
      *
      * @return JsonResponse
      */
     public function update(WalletRequest $request, Wallet $wallet): JsonResponse
     {
-        $validated = $request->validated();
-
-        return response()->json($wallet->update([
-            'wallet_type_id' => $validated['wallet_type_id'],
-            'name' => $validated['name'],
-            'balance' => $validated['balance'],
-            'balance_date' => $validated['balance_date'],
-            'icon' => $validated['icon'],
-        ]));
+        return response()->json($this->walletService->update($request->validated(), $wallet), Response::HTTP_ACCEPTED);
     }
 
     /**
@@ -110,6 +79,7 @@ class WalletController extends Controller
     public function destroy(Wallet $wallet): JsonResponse
     {
         $wallet->delete();
+
         return response()->json([], Response::HTTP_NO_CONTENT);
     }
 }
