247cf4963b8c15a29086558ef0071a482e144fb8
NONstop <non_2002@mail.ru>
2021-08-17 11:32:38 +0300

    fix TransactionRequest.php to change validation

--- api/app/Http/Requests/TransactionRequest.php
+++ api/app/Http/Requests/TransactionRequest.php
@@ -24,10 +24,10 @@ class TransactionRequest extends FormRequest
     public function rules()
     {
         return [
-            'wallet_from_id' => 'required|integer|exists:transactions',
-            'wallet_to_id' => 'required|integer|exists:transactions',
-            'transaction_type_id' => 'required|integer|exists:transactions',
-            'category_id' => 'required|integer',
+            'wallet_from_id' => 'required|integer|exists:transactions,id',
+            'wallet_to_id' => 'nullable|integer|exists:transactions,id',
+            'transaction_type_id' => 'required|integer|exists:transaction_types,id',
+            'category_id' => 'nullable|integer|exists:categories,id',
             'amount' => 'required|numeric',
             'transacted_at' => 'required|date',
         ];
