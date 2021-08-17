5ebcdc36cac585c4994eea3ad1623bf3e84bea7d
NONstop <non_2002@mail.ru>
2021-08-17 22:57:04 +0300

    fix TransactionType.php to use HasManyTransactions trait

--- api/app/Models/TransactionType.php
+++ api/app/Models/TransactionType.php
@@ -2,18 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Relations\Transaction\HasManyTransactions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TransactionType extends Model
 {
     use HasFactory;
+    use HasManyTransactions;
 
     protected $guarded = [];
-
-    public function transactions(): HasMany
-    {
-        return $this->hasMany(Transaction::class);
-    }
 }
