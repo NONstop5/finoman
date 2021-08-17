74e078758128646e5621b26b5ce5e53ce7c32b6f
NONstop <non_2002@mail.ru>
2021-08-17 22:55:57 +0300

    fix Category.php to use HasManyTransactions trait and softDelete

--- api/app/Models/Category.php
+++ api/app/Models/Category.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
+use App\Models\Relations\Transaction\HasManyTransactions;
 use App\Models\Relations\User\BelongsToUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     use BelongsToUser;
+    use HasManyTransactions;
 
     protected $guarded = [];
 
