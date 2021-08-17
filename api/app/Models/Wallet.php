a40e6fecfcae30890f50f8bc041eff14e14d38ca
NONstop <non_2002@mail.ru>
2021-08-17 22:57:31 +0300

    fix Wallet.php to use HasManyTransactions trait and softDelete

--- api/app/Models/Wallet.php
+++ api/app/Models/Wallet.php
@@ -2,25 +2,24 @@
 
 namespace App\Models;
 
+use App\Models\Relations\Transaction\HasManyTransactions;
 use App\Models\Relations\User\BelongsToUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Wallet extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     use BelongsToUser;
+    use HasManyTransactions;
 
     protected $guarded = [];
 
     public function currency(): BelongsTo
     {
         return $this->belongsTo(Currency::class);
-    }
-
-    public function transactions()
-    {
-        return $this->hasMany(Transaction::class);
     }
 }
