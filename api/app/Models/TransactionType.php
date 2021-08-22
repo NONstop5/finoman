7b0182f89d9cb9255d46c6b2f52be85a9480eb3c
NONstop <non_2002@mail.ru>
2021-08-22 17:09:11 +0300

    fix models to add phpdoc descriptions

--- api/app/Models/TransactionType.php
+++ api/app/Models/TransactionType.php
@@ -3,9 +3,31 @@
 namespace App\Models;
 
 use App\Models\Relations\Transaction\HasManyTransactions;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\TransactionType
+ *
+ * @property int $id
+ * @property string $name Название типа транзакции
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection|Transaction[] $transactions
+ * @property-read int|null $transactions_count
+ * @method static Builder|TransactionType newModelQuery()
+ * @method static Builder|TransactionType newQuery()
+ * @method static Builder|TransactionType query()
+ * @method static Builder|TransactionType whereCreatedAt($value)
+ * @method static Builder|TransactionType whereId($value)
+ * @method static Builder|TransactionType whereName($value)
+ * @method static Builder|TransactionType whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class TransactionType extends Model
 {
     use HasFactory;
