9f4bf5f15e0515f056ec607257effa615bc5d384
NONstop <non_2002@mail.ru>
2021-12-13 10:51:01 +0300

    fix api.php to use groupName macros

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WalletController;
+use App\Services\ApiDocService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,28 +18,54 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
 
-Route::group(
-    ['middleware' => ['auth:sanctum']],
+Route::get(
+    '/get-api-doc-data',
+    function (ApiDocService $apiDocService) {
+        $routes = $apiDocService->getRoutes();
+        $apiDocData = $apiDocService->getApiDocData($routes);
+
+        return response()->json($apiDocData);
+    }
+);
+
+Route::groupName('User')->group([],
     function () {
-        Route::apiResource('wallets', WalletController::class);
-        Route::apiResource('categories', CategoryController::class);
-        Route::apiResource('transactions', TransactionController::class);
+        Route::post('/register', [UserController::class, 'register']);
+        Route::post('/login', [UserController::class, 'login']);
+    }
+);
 
-        Route::get(
-            '/user',
-            function (Request $request) {
-                return $request->user();
+Route::middleware(['auth:sanctum'])->group(
+    function () {
+        Route::groupName('Wallets')->apiResource('wallets', WalletController::class);
+        Route::groupName('Categories')->apiResource('categories', CategoryController::class);
+        Route::groupName('Transactions')->apiResource('transactions', TransactionController::class);
+
+        Route::groupName('User')->group([], function () {
+            Route::get(
+                '/user',
+                function (Request $request) {
+                    return $request->user();
+                }
+            );
+            Route::get(
+                '/secrets',
+                function (Request $request) {
+                    return $request->user()->secrets;
+                }
+            );
+            Route::post('/logout', [UserController::class, 'logout']);
+        });
+
+        Route::post(
+            '/get-api-doc-data',
+            function (ApiDocService $apiDocService) {
+                $routes = $apiDocService->getRoutes();
+                $apiDocData = $apiDocService->getApiDocData($routes);
+
+                return response()->json($apiDocData);
             }
         );
-        Route::get(
-            '/secrets',
-            function (Request $request) {
-                return $request->user()->secrets;
-            }
-        );
-        Route::post('/logout', [UserController::class, 'logout']);
     }
 );
