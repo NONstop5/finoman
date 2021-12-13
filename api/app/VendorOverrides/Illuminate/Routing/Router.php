b7f1749d7ad2095cd1708691895f81c90ba57107
NONstop <non_2002@mail.ru>
2021-12-13 12:18:48 +0300

    add Router.php

---
+++ api/app/VendorOverrides/Illuminate/Routing/Router.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\VendorOverrides\Illuminate\Routing;
+
+class Router extends \Illuminate\Routing\Router
+{
+    /**
+     * Get group name from group stack.
+     *
+     * @return string
+     */
+    // public function getGroupName(): string
+    // {
+    //     return $this->groupStack['groupName'];
+    // }
+
+    /**
+     * Set group name to group stack.
+     *
+     * @param string $name
+     */
+    // public function setGroupName(string $name): void
+    // {
+    //     $this->groupStack['groupName'] = $name;
+    // }
+}
